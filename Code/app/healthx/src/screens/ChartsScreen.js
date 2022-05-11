63d0c3c966e05d0ee3a4715bd25872b7657a5837
AlirezaT99 <tajmirriahia@yahoo.com>
2022-05-11 11:37:52 +0430

    Add ResponsiveContainer to charts

--- Code/app/healthx/src/screens/ChartsScreen.js
+++ Code/app/healthx/src/screens/ChartsScreen.js
@@ -10,7 +10,7 @@ import axios from "axios";
 import { patientRecordsURL } from "../api/base";
 import { Ionicons } from "@expo/vector-icons";
 import { useTheme, themeColor } from "react-native-rapi-ui";
-import { AreaChart, Area, LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+import { AreaChart, Area, LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, ResponsiveContainer } from "recharts";
 
 const { width: deviceWidth, height: deviceHeight } = Dimensions.get("screen");
 const chartWidthRatio = 0.8
@@ -94,39 +94,46 @@ class ChartsScreen extends Component {
 	};
 
 	cleanData = (data) => {
+		for (let item of data) {
+			item['date'] = this.getISODate(new Date(item['timestamp']))
+		}
 		return data;
 	}
 
 	renderLineChart = (data) => (
-		<LineChart width={deviceWidth * chartWidthRatio} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-			<Line type="monotone" dataKey="oxygen_saturation" stroke="#8884d8" />
-			<CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-			<XAxis dataKey="timestamp" />
-			<YAxis />
-			<Tooltip />
-		</LineChart>
+		<ResponsiveContainer width="100%" height={250}>
+			<LineChart data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+				<Line type="monotone" dataKey="oxygen_saturation" stroke="#8884d8" />
+				<CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+				<XAxis dataKey="date" />
+				<YAxis />
+				<Tooltip />
+			</LineChart>
+		</ResponsiveContainer>
 	);
 
 	renderAreaChart = (data) => (
-		<AreaChart width={deviceWidth * chartWidthRatio} height={300} data={data}
-			margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
-			<defs>
-				<linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-					<stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
-					<stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
-				</linearGradient>
-				<linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-					<stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
-					<stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
-				</linearGradient>
-			</defs>
-			<XAxis dataKey="timestamp" />
-			<YAxis />
-			<CartesianGrid strokeDasharray="3 3" />
-			<Tooltip />
-			<Area type="monotone" dataKey="body_temperature" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
-			<Area type="monotone" dataKey="environment_temperature" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
-		</AreaChart>
+		<ResponsiveContainer width="100%" height={300}>
+			<AreaChart data={data}
+				margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+				<defs>
+					<linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+						<stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+						<stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+					</linearGradient>
+					<linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+						<stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+						<stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+					</linearGradient>
+				</defs>
+				<XAxis dataKey="date" />
+				<YAxis />
+				<CartesianGrid strokeDasharray="3 3" />
+				<Tooltip />
+				<Area type="monotone" dataKey="body_temperature" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+				<Area type="monotone" dataKey="environment_temperature" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
+			</AreaChart>
+		</ResponsiveContainer>
 	);
 
 	render() {
