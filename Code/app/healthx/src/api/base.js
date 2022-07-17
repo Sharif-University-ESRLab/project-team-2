6b30b2192137867432d35774c7fcda99fe24a412
savaw <savaw97@gmail.com>
2022-07-18 02:20:04 +0430

    Add basic app view

--- Code/app/healthx/src/api/base.js
+++ Code/app/healthx/src/api/base.js
@@ -3,7 +3,7 @@
  * which will be used from various parts of the code
  */
 
-const baseURL = 'http://192.168.43.231:8000/api/v1'
+const baseURL = 'http://192.168.43.142:8000/api/v1'
 
 const patientsURL = `${baseURL}/patients/`
 
